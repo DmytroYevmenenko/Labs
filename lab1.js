bf29398638a41fb5ee8e3832fb6ce130b1d97d24
DmytroYevmenenko <evmenenkooor@gmail.com>
2026-02-26 19:13:22 +0200

    2

--- lab1.js
+++ lab1.js
@@ -11,11 +11,12 @@ function* dayGenerator() {
 function startTimeout(iterator, seconds) {
     let iteration = 1;
     const interval = setInterval(() => {
-        const day = iterator.next().value;
+        const result = iterator.next();
+        const day = result.value;
         const time = new Date().toLocaleTimeString();
         
         console.log(`[${time}] Iteration ${iteration++}: ${day}`);
-    }, 200);
+    }, 500);
 
     setTimeout(() => {
         clearInterval(interval);
