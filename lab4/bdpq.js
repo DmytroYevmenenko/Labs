780c708f9504fdb9a72e3c707ffe900144d040f2
Dmytro Yevmenenko <evmenenkooor@gmail.com>
2026-04-12 22:06:11 +0300

    4

--- lab4/bdpq.js
+++ lab4/bdpq.js
@@ -2,14 +2,16 @@ class BiDirectionalPriorityQueue {
     constructor() {
         this.items = [];
         this.order = [];
+        this.counter = 0;
     }
 
     enqueue(item, priority) {
         const element = {
             value: item,
             priority: priority,
-            id: Math.random()
+            id: this.counter
         };
+        this.counter++;
         this.items.push(element);
         this.order.push(element);
     }
@@ -52,44 +54,47 @@ class BiDirectionalPriorityQueue {
         if (this.items.length === 0) return null;
 
         let toRemove = null;
+        let removeIndex = -1;
 
         if (mode === "highest") {
             toRemove = this.items[0];
+            removeIndex = 0;
             for (let i = 1; i < this.items.length; i++) {
                 if (this.items[i].priority > toRemove.priority) {
                     toRemove = this.items[i];
+                    removeIndex = i;
                 }
             }
+            this.items.splice(removeIndex, 1);
         } else if (mode === "lowest") {
             toRemove = this.items[0];
+            removeIndex = 0;
             for (let i = 1; i < this.items.length; i++) {
                 if (this.items[i].priority < toRemove.priority) {
                     toRemove = this.items[i];
+                    removeIndex = i;
                 }
             }
+            this.items.splice(removeIndex, 1);
         } else if (mode === "oldest") {
-            toRemove = this.order[0];
+            toRemove = this.order.shift();
+            for (let i = 0; i < this.items.length; i++) {
+                if (this.items[i].id === toRemove.id) {
+                    this.items.splice(i, 1);
+                    break;
+                }
+            }
         } else if (mode === "newest") {
-            toRemove = this.order[this.order.length - 1];
+            toRemove = this.order.pop();
+            for (let i = 0; i < this.items.length; i++) {
+                if (this.items[i].id === toRemove.id) {
+                    this.items.splice(i, 1);
+                    break;
+                }
+            }
         } else {
             return null;
         }
-
-        const newItems = [];
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].id !== toRemove.id) {
-                newItems.push(this.items[i]);
-            }
-        }
-        this.items = newItems;
-
-        const newOrder = [];
-        for (let i = 0; i < this.order.length; i++) {
-            if (this.order[i].id !== toRemove.id) {
-                newOrder.push(this.order[i]);
-            }
-        }
-        this.order = newOrder;
 
         return toRemove;
     }
@@ -100,8 +105,10 @@ pq.enqueue("A", 5);
 pq.enqueue("B", 1);
 pq.enqueue("C", 10);
 pq.enqueue("D", 3);
+pq.enqueue("E", 7);
 
 console.log(pq.dequeue("highest").value);
-console.log(pq.dequeue("oldest").value);
 console.log(pq.dequeue("lowest").value);
+console.log(pq.dequeue("oldest").value);
 console.log(pq.dequeue("newest").value);
+console.log(pq.peek("highest").value);
