d278ce7960b8ac153ed6f9258301f1d6639525f9
Dmytro Yevmenenko <evmenenkooor@gmail.com>
2026-04-20 22:35:17 +0300

    promise

--- lab5/async.js
+++ lab5/async.js
@@ -19,11 +19,33 @@ function asyncMapCallback(arr, fn, callback) {
   }
 }
 
-console.log('=== Callback ===')
+function asyncMapPromise(arr, fn) {
+  return new Promise((resolve, reject) => {
+    const result = []
+    let completed = 0
 
+    if (arr.length === 0) {
+      resolve(result)
+      return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      Promise.resolve(fn(arr[i], i, arr))
+        .then(value => {
+          result[i] = value
+          completed++
+          if (completed === arr.length) resolve(result)
+        })
+        .catch(reject)
+    }
+  })
+}
+
+console.log('=== Callback ===')
 asyncMapCallback([10, 20, 30], (x, i, arr, cb) => {
   setTimeout(() => cb(null, x + 5), 50)
-}, (err, res) => {
-  if (err) console.log(err)
-  else console.log(res)
-})
+}, (err, res) => console.log(res))
+
+console.log('=== Promise ===')
+asyncMapPromise([10, 20, 30], x => x * 3)
+  .then(console.log)
