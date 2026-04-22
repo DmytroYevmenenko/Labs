987c7601c6556e330e271bb0098a509dd9153e90
Dmytro Yevmenenko <evmenenkooor@gmail.com>
2026-04-22 15:12:31 +0300

    add abortable asyncMap

--- lab5/async.js
+++ lab5/async.js
@@ -47,6 +47,49 @@ function asyncMapPromise(arr, fn) {
   })
 }
 
+function asyncMapAbortable(arr, fn, signal) {
+  return new Promise((resolve, reject) => {
+    const result = []
+    let completed = 0
+    let aborted = false
+
+    if (signal && signal.aborted) {
+      reject(new Error('Aborted'))
+      return
+    }
+
+    const onAbort = () => {
+      aborted = true
+      reject(new Error('Aborted'))
+    }
+
+    if (signal) {
+      signal.addEventListener('abort', onAbort)
+    }
+
+    if (arr.length === 0) {
+      resolve(result)
+      return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      Promise.resolve(fn(arr[i], i, arr, signal))
+        .then(value => {
+          if (aborted) return
+          result[i] = value
+          completed++
+          if (completed === arr.length) {
+            if (signal) signal.removeEventListener('abort', onAbort)
+            resolve(result)
+          }
+        })
+        .catch(err => {
+          if (!aborted) reject(err)
+        })
+    }
+  })
+}
+
 console.log('=== Callback ===')
 asyncMapCallback([10, 20, 30], (x, i, arr, cb) => {
   setTimeout(() => cb(null, x + 5), 50)
@@ -57,3 +100,18 @@ console.log('=== Promise with async/await ===')
   const res = await asyncMapPromise([10, 20, 30], x => x * 3)
   console.log(res)
 })()
+
+console.log('=== Abortable ===')
+const controller = new AbortController()
+
+setTimeout(() => controller.abort(), 300)
+
+asyncMapAbortable(
+  [100, 200, 300],
+  async (x, i, arr, signal) => {
+    if (signal && signal.aborted) return
+    await new Promise(r => setTimeout(r, 200))
+    return x / 2
+  },
+  controller.signal
+).catch(console.log)
